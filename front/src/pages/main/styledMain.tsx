8a6efec9bb6c523528bc215b358df3b5ba3154c7
ErinSY <seoyul0203@gmail.com>
2023-03-10 17:50:16 +0900

    WIP

---
+++ front/src/pages/main/styledMain.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const MainContainer = styled.div``;
+
+export const NavContainer = styled.div`
+  font-family: Yeongdo-Rg;
+`;
+
+export const MainTitle = styled.div``;
+
+export const NavBar = styled.div``;
