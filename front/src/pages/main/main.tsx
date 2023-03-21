023bda46fdc4f46d314a8482b9e737b4636e715c
ErinSY <seoyul0203@gmail.com>
2023-03-21 18:12:15 +0900

    메인페이지 수정

--- front/src/pages/main/main.tsx
+++ front/src/pages/main/main.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import * as S from './styledMain';
+import * as S from './StyledMain';
 
 const Main = () => {
   return (
     <S.MainContainer>
-      <S.NavContainer>
-        <S.MainTitle>우당탕탕</S.MainTitle>
-        <S.NavBar>
-          <ul>
-            <span>로그인</span>
-            <span>마이페이지</span>
-            <span>라이더모임</span>
-            <span>경로추천</span>
-          </ul>
-        </S.NavBar>
-      </S.NavContainer>
+      {/* 아래멘트는 임의...바꿀예정 */}
+      <div>
+        <p>연료가격 상승 및 팬데믹 방어, </p>
+        <p>지구 환경까지 지킬수 있는 방법은?</p>
+        <p className="maintitle">자전거!</p>
+      </div>
+
+      <iframe
+        src="https://public.tableau.com/views/UDTT-DataAnalysis/1_1?:language=ko-KR&publish=yes&:display_count=n&:origin=viz_share_link?:showVizHome=no&:embed=true"
+        width="1000"
+        height="80%"
+        title="구역별 자전거이용률"
+      ></iframe>
     </S.MainContainer>
   );
 };
--- front/src/pages/main/styledMain.tsx
+++ front/src/pages/main/styledMain.tsx
@@ -1,11 +1,31 @@
 import styled from 'styled-components';
 
-export const MainContainer = styled.div``;
-
-export const NavContainer = styled.div`
-  font-family: Yeongdo-Rg;
+export const MainContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  padding-bottom: 50px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  > p {
+    font-weight: 900;
+    font-size: 40px;
+    margin-bottom: 20px;
+  }
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-weight: 900;
+    font-size: 25px;
+    margin-bottom: 20px;
+    > .maintitle {
+      font-size: 35px;
+      color: #1ca82a;
+    }
+    > p {
+      padding: 10px;
+    }
+  }
 `;
-
-export const MainTitle = styled.div``;
-
-export const NavBar = styled.div``;
