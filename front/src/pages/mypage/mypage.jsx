ac6febb2cbf1f55e30af10ec515765d81d38cfb7
ErinSY <seoyul0203@gmail.com>
2023-03-21 18:15:10 +0900

    주석추가 & myinfo 컴포넌트 분리

--- front/src/pages/mypage/mypage.jsx
+++ front/src/pages/mypage/mypage.jsx
@@ -1,16 +1,16 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-
+import Myinfo from './myinfo/myinfo';
 import Clublist from './clublist/Clublist';
 import * as S from './StyledMypage';
 
 export default function MyPage() {
   const navigate = useNavigate();
-  const [customerInfo, setCustomerInfo] = useState({});
+  const [customerInfo, setCustomerInfo] = useState('');
   const [currentTab, setCurrentTab] = useState('clubList');
   const [currentSubTab, setCurrentSubTab] = useState('join');
-  const [nickname, setNickname] = useState('join');
+  const [nickname, setNickname] = useState('');
   const [joinList, setJoinList] = useState([]);
   const [myClubList, setMyClubList] = useState([]);
   const [myCommentList, setMyCommentList] = useState([]);
@@ -22,13 +22,36 @@ export default function MyPage() {
   //     .then(res => setCustomerInfo(res.data));
   // }, []);
 
+  ///////////// test용_사용자정보가져오기  //////////////
+  useEffect(() => {
+    axios.get('/data/customerInfo.json').then(res => {
+      setCustomerInfo(res.data);
+      setJoinList(res.data.join_list);
+      setMyClubList(res.data.my_group_list);
+      setMyCommentList(res.data.comment_list);
+    });
+  }, []);
+  ///////////// test용_사용자정보가져오기  //////////////
+
+  ///////////// test용_모임리스트정보가져오기  //////////////
+  const getJoinListData = () => {
+    axios.get('/data/joinList.json').then(res => setJoinList(res.data));
+  };
+  ///////////// test용_모임리스트정보가져오기  //////////////
+
   const submitChangedNickname = event => {
     event.preventDefault();
-    let userNickname = { user_name: nickname };
+    let userNickname = { nickname: nickname };
     console.log(userNickname);
+    alert('닉네임이 변경되었습니다.');
+    setNickname('');
+
     // axios
     //   .patch(`http://localhost:5001/users/${userId}`, userNickname)
-    //   .then(() => alert('닉네임이 변경되었습니다.'));
+    //   .then(() => {
+    //     alert('닉네임이 변경되었습니다.');
+    //     setNickname('');
+    //   });
   };
 
   const signout = () => {
@@ -39,12 +62,17 @@ export default function MyPage() {
 
   return (
     <S.Container>
+      {/* 웰컴메세지 & 닉네임 */}
       <S.WelcomeMsg>
         <S.AccountIcon className="mat-icon" />
-        {/* 닉네임부분 변경예정 */}
-        <p>안녕하세요 {customerInfo.name} 고객님 </p>
+        <p>
+          안녕하세요 '<span className="nickname"> {customerInfo.nickname}</span>{' '}
+          ' 님
+        </p>
         <p>오늘도 환영합니다!</p>
       </S.WelcomeMsg>
+
+      {/* 주메뉴칼럼 데이터*/}
       <S.MenuTab>
         <ul className="tabs">
           <li
@@ -65,71 +93,91 @@ export default function MyPage() {
           </li>
         </ul>
       </S.MenuTab>
+
+      {/* 서브메뉴칼럼 데이터*/}
       {currentTab === CURRENT_TAB.CLUB_LIST && (
         <S.Subtitle>
           <div>
-            <p onClick={() => setCurrentSubTab(CURRENT_SUBTAB.JOIN)}>참여</p>
-            <p onClick={() => setCurrentSubTab(CURRENT_SUBTAB.MY_CLUB)}>진행</p>
-            <p onClick={() => setCurrentSubTab(CURRENT_SUBTAB.MY_COMMENT)}>
+            <p
+              onClick={() => {
+                setCurrentSubTab(CURRENT_SUBTAB.JOIN);
+              }}
+            >
+              참여
+            </p>
+            <p
+              onClick={() => {
+                setCurrentSubTab(CURRENT_SUBTAB.MY_CLUB);
+              }}
+            >
+              진행
+            </p>
+            <p
+              onClick={() => {
+                setCurrentSubTab(CURRENT_SUBTAB.MY_COMMENT);
+              }}
+            >
               코멘트
             </p>
           </div>
           <S.Line widthLength="50%" />
         </S.Subtitle>
       )}
+
+      {/* 내정보관리 */}
       {currentTab === CURRENT_TAB.MY_INFO && (
-        <>
-          <S.Subtitle>
-            <p>나의 정보 관리</p>
-            <S.Line widthLength="50%" />
-          </S.Subtitle>
-          <S.FormContainer onSubmit={submitChangedNickname}>
-            <div className="form-field">
-              <label htmlFor="nickname">닉네임</label>
-              <input
-                required
-                id="nickname"
-                type="text"
-                name="nickname"
-                placeholder="변경할 닉네임을 입력해 주세요"
-                onChange={e => {
-                  setNickname(e.target.value);
-                }}
-              />
-            </div>
-            <S.StyledButton>변경하기</S.StyledButton>
-          </S.FormContainer>
-          <S.SignoutDiv>
-            <p onClick={signout}>회원탈퇴</p>
-          </S.SignoutDiv>
-        </>
+        <Myinfo
+          submitEvent={submitChangedNickname}
+          setNicknameEvent={setNickname}
+          signoutEvent={signout}
+          nicknameValue={nickname}
+          customerInfo={customerInfo}
+        />
       )}
+
+      {/* 내모임관리 */}
+
+      {/* 모임_참여모임 */}
       {currentSubTab === CURRENT_SUBTAB.JOIN && (
         <S.ShowList>
-          {joinList.length === 0 ? '참여한 모임이 없습니다.' : <Clublist />}
+          {joinList.length === 0 ? (
+            '참여한 모임이 없습니다.'
+          ) : (
+            <Clublist data={joinList} />
+          )}
         </S.ShowList>
       )}
+
+      {/* 모임_주최모임 */}
       {currentSubTab === CURRENT_SUBTAB.MY_CLUB && (
         <S.ShowList>
-          {joinList.length === 0 ? '주최한 모임이 없습니다.' : <Clublist />}
+          {myClubList.length === 0 ? '주최한 모임이 없습니다.' : <Clublist />}
         </S.ShowList>
       )}
+
+      {/* 모임_코멘트 */}
       {currentSubTab === CURRENT_SUBTAB.MY_COMMENT && (
         <S.ShowList>
-          {joinList.length === 0 ? '등록한 댓글이 없습니다.' : <Clublist />}
+          {myCommentList.length === 0 ? (
+            '등록한 댓글이 없습니다.'
+          ) : (
+            <Clublist />
+          )}
         </S.ShowList>
       )}
     </S.Container>
   );
 }
 
+// 탭메뉴(주) 상수데이터
 const CURRENT_TAB = {
   CLUB_LIST: 'clubList',
   MY_INFO: 'myinfo',
 };
 
+// 탭메뉴(서브) 상수데이터
 const CURRENT_SUBTAB = {
-  JOIN: 'clubList',
+  JOIN: 'join',
   MY_CLUB: 'myinfo',
   MY_COMMENT: 'comment',
 };
